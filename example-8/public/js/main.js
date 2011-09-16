c04a7a94db199982db6c1dd5fc075ec04c45df5d
Vladimir Dronnikov <dronnikov@gmail.com>
2011-09-16 20:14:02 +0400

    flash

--- example-8/public/js/main.js
+++ example-8/public/js/main.js
@@ -12,21 +12,21 @@ require([
     // FF >= 7.0
     (browser.mozilla && +browser.version >= 7) ||
     // Chrome >= 14
-    (browser.webkit && +browser.version >= 535.1)
+    (browser.webkit && +browser.version > 535.1)
   )) {
     main();
   // provide shim for older browsers
   } else {
     // N.B. let shim always apply, nomatter there is native WebSocket
     WebSocket = false;
-    window.WEB_SOCKET_DEBUG = true
+    ///window.WEB_SOCKET_DEBUG = true;
     window.WEB_SOCKET_SWF_LOCATION = 'js/flash/WebSocketMain.swf';
     // FIXME: web_socket.js's way of attaching onload event clashes with
     // requirejs.
     // See https://github.com/kanaka/web-socket-js/commit/978e31ce6ff15926391b616667c4e4370bbac800#commitcomment-590868
     window.WEB_SOCKET_DISABLE_AUTO_INITIALIZATION = true;
     require(['order!flash/swfobject', 'order!flash/web_socket'], function() {
-      WebSocket.loadFlashPolicyFile('xmlsocket://localhost:888');
+      ///WebSocket.loadFlashPolicyFile('xmlsocket://localhost:443');
       WebSocket.__initialize();
       main();
     });
